d16fca42ca6bba79fe4d537c794007d5e33def53
Yago Ananias <yago_souza92@yahoo.com.br>
2021-04-05 18:56:23 -0300

    Conecting H2 database

--- src/main/java/br/com/yagoananias/mybooks/MybooksApplication.java
+++ src/main/java/br/com/yagoananias/mybooks/MybooksApplication.java
@@ -1,13 +1,40 @@
 package br.com.yagoananias.mybooks;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.yagoananias.mybooks.domain.Categoria;
+import br.com.yagoananias.mybooks.domain.Livro;
+import br.com.yagoananias.mybooks.repositories.CategoriaRepository;
+import br.com.yagoananias.mybooks.repositories.LivroRepository;
+
 @SpringBootApplication
-public class MybooksApplication {
+public class MybooksApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MybooksApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de Tecnologia");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
